e88b826ea5be4c23156f92b047d742ceb6933d37
Bəxtiyar <bakhtiyar.garashov@gmail.com>
2024-08-08 23:18:19 +0300

    Develop (#4)
    * Use cargo workspaces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use cli::say_hi;
+
 fn main() {
-    println!("Hello, world!");
+    say_hi()
+}
+
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = cli::add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
